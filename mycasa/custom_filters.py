a8b2c01145f3d38d4ab20dc5241134c78f0f5e1d
Taro302 <128880322+Taro302@users.noreply.github.com>
2024-04-06 19:47:09 -0500

    Delete mycasa directory

--- mycasa/custom_filters.py
+++
@@ -1,9 +0,0 @@
-from django import template
-from django.template.defaultfilters import floatformat
-
-register = template.Library()
-
-@register.filter
-def format_number(value):
-    return floatformat(value, 0).replace(',', '.')
-
--- mycasa/templatetags/custom_filters.py
+++
@@ -1,7 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-def add_commas(value):
-    return "{:,.2f}".format(value)
